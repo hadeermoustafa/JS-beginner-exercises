c4016394b3956eb8ceb9ce7eab9b4b013f95abdc
hadeermoustafa <hadeer.moustafa@outlook.com>
2022-09-05 00:18:11 +0200

    Update script.js

--- Fizz Buzz/script.js
+++ Fizz Buzz/script.js
@@ -1,1 +1,20 @@
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+for (i =1; i <= answer; i ++){
+    if (i%3===0 && i%5===0){
+        console.log('fizzbuzz');
+    } 
+
+    else if(i%3 === 0){
+        console.log('fizz');
+    }
+    
+    else if (i%5 ===0){
+
+    console.log('buzz');
+    }
+    
+    else{
+        console.log(i);
+    }
+}
 
